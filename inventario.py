75dc3e0d0a242b75416622bfb3a9dfc15b708938
efrain <hlaluz59@gmail.com>
2026-03-13 10:03:03 -0500

    data_entry(Python)

--- inventario.py
+++ inventario.py
@@ -1,1 +1,23 @@
- 
+
+def name():
+    try:
+        products1 = (input("What product do you to sell?: "))
+        price()
+    except ValueError:
+        print("Error")
+
+def price():
+        try:
+            price1 = float(input("enter the price of the product: "))
+            quantity()
+        except ValueError:
+            print("Error, just enter numbers.")
+            price()
+
+def quantity(): 
+        try:
+            quantity1 = int(input("How many would you like purchase?: "))
+        except ValueError:
+            print("Error, just enter integer numbers.")
+            quantity()
+
