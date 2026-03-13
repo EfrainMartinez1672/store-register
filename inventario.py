dbb8cb9fd9c7b4a881ea9950c492de93438bab4c
efrain <hlaluz59@gmail.com>
2026-03-13 10:50:18 -0500

    Display_results

--- inventario.py
+++ inventario.py
@@ -1,5 +1,5 @@
 update = []
-total_cost2 = []
+
 def name():
     products1 = str(input("What product do you to sell?: "))
     price(products1)
@@ -10,15 +10,18 @@ def price(products1):
             quantity(products1, price1)
         except ValueError:
             print("Error, just enter numbers.")
-            price()
+            price(products1)
 
 def quantity(products1, price1): 
         try:
             quantity1 = int(input("How many would you like purchase?: "))
             total_cost = quantity1 * price1
-            total_cost2.append(total_cost)
-            update.append(f"name the product: {products1} | unit price: {price1} | quantity: {quantity1}")
+            update.append(f"name the product: {products1} | unit price: {price1} | quantity: {quantity1} | total cost: {total_cost}")
         except ValueError:
             print("Error, just enter integer numbers.")
-            quantity()
+            quantity(products1, price1)
+name()
+for dc in update:
+    print(dc)
+
 
