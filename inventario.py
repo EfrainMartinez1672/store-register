6474927190a547050523fa62bace78853f271a18
efrain <hlaluz59@gmail.com>
2026-03-13 10:35:08 -0500

    mathematical_operations

--- inventario.py
+++ inventario.py
@@ -1,22 +1,23 @@
-
+update = []
+total_cost2 = []
 def name():
-    try:
-        products1 = (input("What product do you to sell?: "))
-        price()
-    except ValueError:
-        print("Error")
+    products1 = str(input("What product do you to sell?: "))
+    price(products1)
 
-def price():
+def price(products1):
         try:
             price1 = float(input("enter the price of the product: "))
-            quantity()
+            quantity(products1, price1)
         except ValueError:
             print("Error, just enter numbers.")
             price()
 
-def quantity(): 
+def quantity(products1, price1): 
         try:
             quantity1 = int(input("How many would you like purchase?: "))
+            total_cost = quantity1 * price1
+            total_cost2.append(total_cost)
+            update.append(f"name the product: {products1} | unit price: {price1} | quantity: {quantity1}")
         except ValueError:
             print("Error, just enter integer numbers.")
             quantity()
